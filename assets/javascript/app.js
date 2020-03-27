c7655612ba949fc5db9f7541b2ac62438bc20aa4
Firecraker <mezamic000@gmail.com>
2020-03-26 20:11:35 -0500

    rewrote my questions,choices, and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,47 +2,71 @@ $(document).ready(function() {
 	var correct = 0;
 	var incorrect = 0;
 	var unanswered = 0;
-	var trivia = {
-		questions: {
-			q1: "A man named Michel Lotito once ate an entire...?",
-			q2: "On Jupiter and Saturn, it rains...?",
-			q3: "In 16th century France, 400 people died from which plaque?",
-			q4: "What U.S state is the closest to Africa?",
-			q5: "What was the first thing ever bought and sold on the Internet?",
-			q6: "How long does it take to make a Jelly Bean?",
-			q7: "The CIA once tried to make what species of animal into spies??",
-			q8: "Boston was nearly destroyed by what substance in 1919? ",
-			q9: "If you live in Brooklyn, you can rent...?",
-			q10: "What was Mickey Mouse called originally? "
+	var trivia = [
+		{
+			question: "A man named Michel Lotito once ate an entire...?",
+			choices: ["Airplane", "Blue Whale", "Pack of Cigarettes", "Pizza"],
+			answer: 0
 		},
-		choices: {
-			q1: ["Airplane", "Blue Whale", "Pack of Cigarettes", "Pizza"],
-			q2: ["Cats and Dogs", "Diamonds", "Asteroids", "Sand"],
-			q3: [
+
+		{
+			question: "On Jupiter and Saturn, it rains...?",
+			choices: ["Cats and Dogs", "Diamonds", "Asteroids", "Sand"],
+			answer: 1
+		},
+
+		{
+			question: "In 16th century France, 400 people died from 		which plaque?",
+			choices: [
 				"The Dancing Plague",
 				"The Black Death",
 				"The Third Plague",
 				"The Twirling Plaque"
 			],
-			q4: ["Colorado", "New York", "Texas", "Maine"],
-			q5: ["Rubber Duck", "Armoured Tank", "Bag of Weed", "Human Skull"],
-			q6: ["a Few Hours", "a Week", "6 Months", "a Year"],
-			q7: ["cats", "spiders", "alligators", "moles"],
-			q8: ["water", "molasses", "dirt", "vinegar"],
-			q9: ["a Mom", "Chickens", "Casket", "Paparazzi"],
-			q10: ["Walt", "Rooney", "Willie", "Mortimer"]
+			answer: 0
 		},
-		answers: {
-			q1: "Airplane",
-			q2: "Diamonds",
-			q3: "The Dancing Plague",
-			q4: "Maine",
-			q5: "Bag of Weed",
-			q6: "a Week",
-			q7: "cats",
-			q8: "molasses",
-			q9: "a Mom",
-			q10: "Mortimer"
+
+		{
+			question: "What U.S state is the closest to Africa?",
+			choices: ["Colorado", "New York", "Texas", "Maine"],
+			answer: 3
+		},
+
+		{
+			question:
+				"What was the first thing ever bought and sold on 		the Internet?",
+			choices: ["Rubber Duck", "Armoured Tank", "Bag of Weed", "Human Skull"],
+			answer: 2
+		},
+
+		{
+			question: "How long does it take to make a Jelly Bean?",
+			choices: ["a Few Hours", "a Week", "6 Months", "a Year"],
+			answer: 1
+		},
+
+		{
+			question: "The CIA once tried to make what species of animal 	into spies?",
+			choices: ["cats", "spiders", "alligators", "moles"],
+			answer: 0
+		},
+
+		{
+			question: "Boston was nearly destroyed by what substance in 	1919? ",
+			choices: ["water", "molasses", "dirt", "vinegar"],
+			answer: 1
+		},
+
+		{
+			question: "What unusual service can you pay for if you live 	in Brooklyn?",
+			choices: ["Temporary Mom", "Cuddeler", "Ambassador", "Paparazzi"],
+			answer: 0
+		},
+
+		{
+			question: "What was Mickey Mouse called originally? ",
+			choices: ["Walt", "Rooney", "Willie", "Mortimer"],
+			answer: 3
 		}
-	};
+	];
 });
